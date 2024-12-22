12a2e9997463a55379fe254b0a848ea4d10e05e8
Ksenia Khachaturyan <yukina@onreza.ru>
2024-12-23 00:35:00 +0300

    Добавила функции для обработки словарей

--- src/widget/widget.py
+++ src/widget/widget.py
@@ -2,13 +2,22 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
+    """
+    Функция для маскировки счетов и кард
+    :param account_card: счет или карта
+    :return: Возвращает зашифрованный счет или карту
+    """
     if account_card[0:4] == "Счет" or account_card[0:4] == "Счёт":
-        mask_card: str = account_card.replace(account_card[-20:], get_mask_account(account_card[-20:]))
+        return account_card.replace(account_card[-20:], get_mask_account(account_card[-20:]))
     else:
-        mask_card: str = account_card.replace(account_card[-16:], get_mask_card_number(account_card[-16:]))
-    return mask_card
+        return account_card.replace(account_card[-16:], get_mask_card_number(account_card[-16:]))
 
 
 def get_date(date: str) -> str:
+    """
+    Функция для преобразования даты из одного формата в другой
+    :param date: дата
+    :return: возвращает дату в формате "ДД.ММ.ГГГГ"
+    """
     date_split = date[0:10].split("-")
     return f"{date_split[2]}.{date_split[1]}.{date_split[0]}"
