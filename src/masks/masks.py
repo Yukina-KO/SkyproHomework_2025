778daac6149e21bed00503cd85eebe114b4774a3
Yukina-KO <kseniya.khachaturyan2000@gmail.com>
2025-02-09 04:16:46 +0300

    Создала logs для masks и utils

--- src/masks/masks.py
+++ src/masks/masks.py
@@ -1,13 +1,22 @@
+import logging
+
+logger = logging.getLogger("src.masks")
+
+
 def get_mask_card_number(card_number: str) -> str:
     """
     Функция преобразования номера карты в "полный" зашифрованный номер
     :param card_number: номер карты пользователя
     :return: зашифрованный номер карты пользователя
     """
+    logger.info(f"Попытка маскировки номера карты: {card_number}")
     card_number_str = str(card_number)
     if len(card_number_str) == 16 and card_number_str.isdigit():
-        return f"{card_number_str[0:5]} {card_number_str[4:6]}** **** {card_number_str[12:17]}"
+        masked = f"{card_number_str[0:5]} {card_number_str[4:6]}** **** {card_number_str[12:17]}"
+        logger.debug(f"Результат маскировки: {masked}")
+        return masked
     else:
+        logger.warning(f"Некорректный номер карты: {card_number}")
         return "Некорректный номер"
 
 
@@ -17,8 +26,12 @@ def get_mask_account(account_number: str) -> str:
     :param account_number: номер счета пользователя
     :return: зашифрованный номер счета пользователя
     """
+    logger.info(f"Попытка маскировки номера счёта: {account_number}")
     account_number_str = str(account_number)
     if len(account_number_str) == 20 and account_number_str.isdigit():
-        return "**" + account_number_str[-4:]
+        masked = "**" + account_number_str[-4:]
+        logger.debug(f"Результат маскировки: {masked}")
+        return masked
     else:
+        logger.warning(f"Некорректный номер счёта: {account_number}")
         return "Некорректный номер"
--- src/utils/transaction.py
+++ src/utils/transaction.py
@@ -1,13 +1,23 @@
 import json
 import os
+import logging
+from typing import Any
+
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+logger = logging.getLogger("src.utils")
 
 
-def get_transactions() -> None:
+def get_transactions() -> Any:
     file_path = os.path.join(BASE_DIR, "data", "operations.json")
+    logger.info(f"Читаем транзакции из файла: {file_path}")
 
-    with open(file_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-
-    return data
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            logger.info(f"Успешно считано транзакций: {len(data)}")
+        return data
+    except FileNotFoundError:
+        logger.error(f"Файл не найден: {file_path}")
+    except json.JSONDecodeError as err:
+        logger.error(f"Ошибка парсинга JSON: {err}")
